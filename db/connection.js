5c97226914e5b0086cd909adc09d6c5103cc21bc
JessicaAKelley <kelleyajessica@gmail.com>
2021-09-08 10:51:36 -0500

    Connection update

--- db/connection.js
+++ db/connection.js
@@ -5,7 +5,7 @@ const db = mysql.createConnection({
     // Your MySQL username,
     user: 'root',
     // Your MySQL password
-    password: 'C3d@R1205!@#',
+    password: 'password',
     database: 'election'
 });
 
